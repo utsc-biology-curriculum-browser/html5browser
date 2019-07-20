3d5670ca2364a7bfbbc97b5b7782d56ca440aae7
sasoripathos <sasori.pan.jerry@gmail.com>
2019-07-20 17:32:33 -0400

    Course informations

--- scraping/base.js
+++ scraping/base.js
@@ -9,3 +9,5 @@ exports.domainName = "https://utsc.calendar.utoronto.ca/";
 
 // Infor needed for scrap program list
 exports.programs = JSON.parse(fs.readFileSync(path.join(__dirname, 'data/rbPrograms.json')));
+// Infor needed for scrap course information
+exports.courseRules = JSON.parse(fs.readFileSync(path.join(__dirname, 'data/rbCourses.json')));
