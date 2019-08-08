94eb12f57e29b8d4d9463c862337db335c373ddf
Zhili (Jerry) Pan <sasori.pan.jerry@gmail.com>
2019-08-07 21:54:05 -0400

    Use compound node and seperate courses by year
    * Ready to keep digging into compound nodes
    
    * mj-pbs and mi-bs updated
    
    * mj-mbids
    
    * mj-bs, mj-hbs
    
    * Fix mj-hbs
    
    * mj-cbs
    
    * Little fix
    
    * Minor fix about information
    
    * Sp-ibs
    
    * sp-hbs
    
    * Sp-cbs

--- data/proc.js
+++ data/proc.js
@@ -3,10 +3,65 @@ const path = require('path');
 
 let mapInfo = JSON.parse(fs.readFileSync(path.join(__dirname, 'mapInfo-original.json')));
 
+let pos = null;
+function initPos () {
+    pos = {
+        'First Year': {
+            x: 0,
+            y: 0
+        },
+        'Second Year': {
+            x: 0,
+            y: 75
+        },
+        'Third Year': {
+            x: 0,
+            y: 150
+        },
+        'Third/Fourth Year': {
+            x: 0,
+            y: 225
+        },
+        'Fourth Year': {
+            x: 0,
+            y: 300
+        },
+        'Ecology and Evolution': {
+            x: 0,
+            y: 225
+        },
+        'Organismal Biology':  {
+            x: 0,
+            y: 300
+        },
+        'PBN':  {
+            x: 0,
+            y: 300
+        },
+        'CEC':  {
+            x: 0,
+            y: 375
+        },
+        'CGD':  {
+            x: 0,
+            y: 450
+        },
+        'OB':  {
+            x: 0,
+            y: 520
+        },
+    };
+}
+
  /* -- Add nodes -- */
-function Node(id, desc, cat) {
+function Node(id, desc, cat, pos) {
     this.group = 'nodes';
-    this.data = {'id': id, 'desc' : desc.slice(0, 6), 'cat': cat};
+    this.data = {
+        'id': id,
+        'desc' : desc,
+        'cat': cat
+    };
+    this.position = pos;
 }
 
 function Edge(id, sourceId, targetId, cat) {
@@ -20,12 +75,14 @@ function Edge(id, sourceId, targetId, cat) {
 }
 
 function constructNode(id, cat) {
-    if(id.length == 8) {
-        return new Node(id, id, cat);
+    let lst = id.split('-');
+    if(id.length == 8 || lst.length == 1) {
+        let desc = id.length == 8 ? id.slice(0, 6) : id;
+        let result = new Node(id, desc, cat, {'x': pos[cat].x*100, 'y': pos[cat].y});
+        pos[cat].x++;
+        return result;
     } else {
-        let lst = id.split('-');
-        return new Node(id, lst[0], cat);
-        // TODO: TBD whether use compound nodes, or add edges
+        return new Node(id, lst[0], cat, {'x': 10000000, 'y': 10000000});
     }
 }
 
@@ -34,31 +91,66 @@ function constructEdge(id, cat) {
     return new Edge(id, pair[1], pair[0], cat);
 }
 
+
+function updateForParent(nodeMap, id) {
+    let lst = id.split('<');
+    if(lst.length < 2) {return false;}
+    let target = lst[0];
+    let source = lst[1];
+    if(target.length == 8) {return false;} // target not a parent
+    nodeMap[source].data.parent = target;
+    nodeMap[target].data.cat = 'parent';
+    nodeMap[target].position.x = nodeMap[target].position.x < nodeMap[source].position.x ?　nodeMap[target].position.x　: nodeMap[source].position.x;
+    nodeMap[target].position.y = nodeMap[target].position.y < nodeMap[source].position.y ?　nodeMap[target].position.y　: nodeMap[source].position.y;
+    return true;
+}
+
 ans = {};
 
 for(let progId in mapInfo) {
+    initPos();
     ans[progId] = {"nodes":[], "edges": []};
-    // For result nodes
-    let ansNodes = ans[progId].nodes;
+
+    //-------------------------------------------------
+    // Node map
     let curNodes = mapInfo[progId].nodes;
     // Map to track nodes
-    /*let nodeMap = {};*/
+    let nodeMap = {};
     for(let year in curNodes) {
         for(let i=0;i<curNodes[year].length;i++) {
             let id=curNodes[year][i];
             let newNode = constructNode(id, year);
-            ansNodes.push(newNode);
+            nodeMap[id] = newNode;
         }
     }
 
     //-------------------------------------------------
-    // For result edges
+    // Edges
     let ansEdges = ans[progId].edges;
     let curEdges = mapInfo[progId].edges;
     for (let cat in curEdges) {
         for(let i=0;i<curEdges[cat].length;i++) {
             let id = curEdges[cat][i];
-            ansEdges.push(constructEdge(id, cat));
+            if(!updateForParent(nodeMap, id)) {
+                ansEdges.push(constructEdge(id, cat));
+            }
+        }
+    }
+
+    //-------------------------------------------------
+    // For result nodes
+    let ansNodes = ans[progId].nodes;
+    for(let year in curNodes) {
+        // Layer node for a year
+        /*let layerNode = new Node(year, year, 'layer');
+        ansNodes.push(layerNode);*/
+        for(let i=0;i<curNodes[year].length;i++) {
+            let id=curNodes[year][i];
+            let resultNode = nodeMap[id];
+            /*if(!resultNode.data.parent) {
+                resultNode.data.parent = year;
+            }*/
+            ansNodes.push(resultNode);
         }
     }
 }
